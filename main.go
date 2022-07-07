c803079c1b0184fefd9618ffb9a065be826aa162
Christian <3003html@gmail.com>
2022-07-07 04:52:48 -0500

    enviroment variable added

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	var err error
+	err := godotenv.Load()
 	if err != nil {
-		panic(err.Error())
+		log.Fatal("Error loading .env file")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
 }
--- models/users.go
+++ models/users.go
@@ -1,14 +1,18 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // User: data of user
 type User struct {
-	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string             `json:"name"`
+	Email     string             `json:"email"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at,omitempty"`
 }
 
 // Users: List of users
