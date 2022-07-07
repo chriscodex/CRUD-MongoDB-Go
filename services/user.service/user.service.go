754897dfc007a016d53ec26f6f7e0e815d52b557
Christian <3003html@gmail.com>
2022-07-06 23:43:50 -0500

    file user.service.go created

---
+++ services/user.service/user.service.go
@@ -0,0 +1,25 @@
+package services
+
+import (
+	m "../../models"
+)
+
+func Create(user m.User) error {
+
+	return nil
+}
+
+func Read() (m.Users, error) {
+
+	return nil, nil
+}
+
+func Update() error {
+
+	return nil
+}
+
+func Delete() error {
+
+	return nil
+}
