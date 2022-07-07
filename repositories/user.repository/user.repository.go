76c75acf669b9370dee33dcf9c764188c2f73e9f
Christian <3003html@gmail.com>
2022-07-06 23:39:17 -0500

    file user.repository updated

--- repositories/user.repository/user.repository.go
+++ repositories/user.repository/user.repository.go
@@ -1,1 +1,25 @@
 package user_repository
+
+import (
+	m "../../models"
+)
+
+func Created(user m.User) error {
+
+	return nil
+}
+
+func Read() m.Users {
+
+	return nil
+}
+
+func Update() error {
+
+	return nil
+}
+
+func Delete() error {
+
+	return nil
+}
