fae025a602bd3a5556df6e9634bb6c6a460270f6
Christian <3003html@gmail.com>
2022-07-07 00:17:16 -0500

    user.service updated

--- services/user.service/user.service.go
+++ services/user.service/user.service.go
@@ -16,18 +16,31 @@ func Create(user m.User) error {
 }
 
 func Read() (m.Users, error) {
+	users, err := userRepository.Read()
 
-	var users m.Users
+	if err != nil {
+		return nil, err
+	}
 
 	return users, nil
 }
 
 func Update(user m.User, userId string) error {
+	err := userRepository.Update(user, userId)
+
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func Delete(userId string) error {
+	err := userRepository.Delete(userId)
+
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
